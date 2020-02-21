83b527fe7aae6cd31bc38b1d64b4b4d5321d5e22
Justin Hammer <justhamr@gmail.com>
2020-02-21 01:34:19 -0700

    Added index and show methods to test runs controller.

--- app/controllers/test_runs_controller.rb
+++ app/controllers/test_runs_controller.rb
@@ -1,2 +1,9 @@
 class TestRunsController < ApplicationController
+    def index
+        @test_runs = TestRun.all
+    end
+    
+    def show
+        @test_run = TestRun.find(params[:id])
+    end
 end
