d574f9b039be41636f4a9af82b4b8c804cdd4cd7
Justin Hammer <justhamr@gmail.com>
2020-02-22 14:59:01 -0700

    Made changes to dashboard to look cleaner. Installed chartkick.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,16 @@
 class DashboardController < ApplicationController
   def index
-    @lastSixAutomationRuns = AutomationRun.last(6)
+    @lastSixAutomationRuns = AutomationRun.order(id: :desc).first(6)
+    
+    passHash = Hash.new
+    @lastSixAutomationRuns.each do |automationRun|
+      id = automationRun.id
+      passes = TestRun.where(status: 'pass', automation_runs_id: id).count
+      fails = TestRun.where(status: 'fail', automation_runs_id: id).count
+      skips = TestRun.where(status: 'skip', automation_runs_id: id).count
+      passHash[id] = [passes, fails, skips]
+    end
+    puts passHash
+    @testsPassed = passHash
   end
 end
